03cda50a1bcaf525b75989839bb6ce4881002c3b
zhuxuetao <zhuxuetao@newstrength.cn>
2021-01-14 13:54:56 +0800

    测试

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "root": true,
+    "env": {
+        // "common": true,
+        "node": true,
+        "browser": false,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "script"
+    },
+    "rules": {
+        "no-console": "error"
+    }
+};
--- src/config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    root: process.cwd(),
-    hostname: "127.0.0.1",
-    port: "3000",
-    compress: /\.(html|js|css|md)/
-}
